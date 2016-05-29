f6290d0179f1b2fe01ba5ee74d5af7a9d14a7c77
private <user@example.com>
2016-05-29 16:44:26 +0100

    Pass data with the real JSON schema

--- js/spec/userbox-spec.js
+++ js/spec/userbox-spec.js
@@ -11,64 +11,65 @@ describe('UI', function() {
     beforeEach(function() {
       setFixtures('<section class="' + userbox + '"></section>');
       validator = { check: function(data) { return data; }};
+      data      = USERDATA;
     });
 
     it('shows the right avatar', function() {
-      data = {'avatar_url': 'octocat.jpeg'};
+      data.avatar_url = 'octocat.jpeg';
       UI.fillUserBox(data, validator);
       expect($(s(avatar)).attr('src')).toEqual('octocat.jpeg');
     });
 
     it('adds the right alt text to the avatar', function() {
-      data = {'name': 'The Octocat'};
+      data.name = 'The Octocat';
       UI.fillUserBox(data, validator);
       expect($(s(avatar)).attr('alt')).toEqual('The Octocat');
     });
 
     it('shows the user name', function() {
-      data = {'name': 'The Octocat'};
+      data.name = 'The Octocat';
       UI.fillUserBox(data, validator);
       expect($(s(username)).text()).toEqual('The Octocat');
     });
 
     it('links the user name to their profile', function() {
-      data = {'html_url': 'http://example.com'};
+      data.html_url = 'http://example.com';
       UI.fillUserBox(data, validator);
       expect($(s(username) + ' a').attr('href')).toEqual('http://example.com');
     });
 
     it('shows company', function() {
-      data = {'company': 'Octocats'};
+      data.company = 'Octocats';
       UI.fillUserBox(data, validator);
       expect($(s(userbox)).html()).toContain('Octocats');
     });
 
     it('shows location', function() {
-      data = {'location': 'Octoplanet'};
+      data.location = 'Octoplanet';
       UI.fillUserBox(data, validator);
       expect($(s(userbox)).html()).toContain('Octoplanet');
     });
 
     it('shows and links to blog', function() {
-      data = {'blog': 'http://example.com'};
+      data.blog = 'http://blog-example.com';
       UI.fillUserBox(data, validator);
-      expect($(s(userbox)).html().split('http://example.com').length - 1).toEqual(2);
+      expect($(s(userbox)).html().split('http://blog-example.com').length - 1).toEqual(2);
     });
 
     it('shows the number of repos', function() {
-      data = {'public_repos': 1};
+      data.public_repos = 1;
       UI.fillUserBox(data, validator);
       expect($(s(userbox)).html()).toContain('1</span>');
     });
 
     it('shows the number of gists', function() {
-      data = {'public_gists': 2};
+      data.public_gists = 2;
       UI.fillUserBox(data, validator);
       expect($(s(userbox)).html()).toContain('2</span>');
     });
 
     it('shows member-since date', function() {
-      data = {'created_at': '2011-01-25T18:44:36Z'};
+      data.created_at = '2011-01-25T18:44:36Z';
       UI.fillUserBox(data, validator);
       expect($(s(userbox)).html()).toContain('January 25, 2011');
     });
--- js/src/userbox.js
+++ js/src/userbox.js
@@ -7,37 +7,41 @@ var UI = (function() {
     },
     monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+  function join(array) {
+    return array.join('');
+  }
+
   function buildAvatar(avatar_url, alt_text) {
-    return ['<img class="', ui.avatar, '" src="', avatar_url, '" alt="',  alt_text, '" width="230" height="230">'].join('');
+    return join(['<img class="', ui.avatar, '" src="', avatar_url, '" alt="',  alt_text, '" width="230" height="230">']);
   }
 
   function buildUserName(html_url, name) {
-    return ['<h2 class="', ui.username, '"><a href="', html_url, '">', name, '</a></h2>'].join('');
+    return join(['<h2 class="', ui.username, '"><a href="', html_url, '">', name, '</a></h2>']);
   }
 
   function buildCompany(company, location) {
-    return ['<p>', company, ', ', location, '</p>'].join('');
+    return join(['<p>', company, ', ', location, '</p>']);
   }
 
   function buildBlog(blog) {
-    return ['<p><strong>Web:</strong> <a href="', blog, '">', blog, '</a></p>'].join('');
+    return join(['<p><strong>Web:</strong> <a href="', blog, '">', blog, '</a></p>']);
   }
 
   function buildCounters(public_repos, public_gists) {
-    return ['<p><strong>Repos:</strong> <span class="circle-border">', public_repos, '</span> <strong>Gists:</strong> <span class="circle-border">', public_gists, '</span></p>'].join('');
+    return join(['<p><strong>Repos:</strong> <span class="circle-border">', public_repos, '</span> <strong>Gists:</strong> <span class="circle-border">', public_gists, '</span></p>']);
   }
 
-  function buildDate(created_at) {
+  function date(created_at) {
     var d = new Date(created_at);
-    return [monthNames[d.getMonth()], ' ', d.getDate(), ', ', d.getFullYear()].join('');
+    return join([monthNames[d.getMonth()], ' ', d.getDate(), ', ', d.getFullYear()]);
   }
 
   function buildMemberSince(created_at) {
-    return ['<p><strong>Member since:</strong> ', buildDate(created_at), '</p>'].join('');
+    return join(['<p><strong>Member since:</strong> <time datetime="', created_at, '">', date(created_at), '</time>.</p>']);
   }
 
   function buildUserBox(data) {
-    return [
+    return join([
       buildAvatar(data.avatar_url, data.name),
       '<div class="user__info">',
       buildUserName(data.html_url, data.name),
@@ -46,7 +50,7 @@ var UI = (function() {
       buildCounters(data.public_repos, data.public_gists),
       buildMemberSince(data.created_at),
       '</div>'
-    ].join('');
+    ]);
   }
 
   ui.fillUserBox = function(data, validator) {
